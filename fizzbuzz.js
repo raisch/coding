3e2f7774e729258ca185f7a8a523c6e01ec9a731
raisch <raisch@gmail.com>
2018-02-22 11:26:47 -0500

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,37 +1,39 @@
 
-let result = ''
+function naiveFizzBuzz() {
+  let result = ''
 
-const lowerBound = 1
-const upperBound = 100
+  const lowerBound = 1
+  const upperBound = 100
 
-const divisibleBy = function (num, divisor) { // memoized
-  const self = divisibleBy
-  self.memo = self.memo || []
-  return self.memo[num] != null
-    ? self.memo[num]
-    : (self.memo[num] = num % divisor == 0)
+  const divisibleBy = function (num, divisor) { // memoized
+    const self = divisibleBy
+    self.memo = self.memo || []
+    return self.memo[num] != null
+      ? self.memo[num]
+      : (self.memo[num] = num % divisor == 0)
+  }
+
+  for (let target = lowerBound; target <= upperBound; target++) {
+    const divisableByThree = target % 3 == 0
+    const divisableByFive = target % 5 == 0
+
+    if (divisableByThree && divisableByFive) {
+      result += 'fizzbuzz '
+      continue
+    }
+
+    if (divisableByThree) {
+      result += 'fizz '
+      continue
+    }
+
+    if (divisableByFive) {
+      result += 'buzz '
+      continue
+    }
+
+    result += `${target} `
+  }
+
+  console.log(result)
 }
-
-for (let target = lowerBound; target <= upperBound; target++) {
-  const divisableByThree = target % 3 == 0
-  const divisableByFive = target % 5 == 0
-
-  if (divisableByThree && divisableByFive) {
-    result += 'fizzbuzz '
-    continue
-  }
-
-  if (divisableByThree) {
-    result += 'fizz '
-    continue
-  }
-
-  if (divisableByFive) {
-    result += 'buzz '
-    continue
-  }
-
-  result += `${target} `
-}
-
-console.log(result)
