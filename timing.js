d3ab298805d6534dd44ca02a5b152c7fcbdeb952
Rob Raisch <raisch@gmail.com>
2018-02-23 17:51:57 -0500

    renamed NUMS -> RANGE

--- timing.js
+++ timing.js
@@ -5,7 +5,7 @@ const FB = require('./lib/fizzbuzz')
 
 const MAX = 100
 
-const NUMS = FB.range(MAX, 1)
+const RANGE = FB.range(MAX, 1)
 
 const MUTATORS = [
   ['Fizz', i => i % 3 == 0 ],
@@ -25,17 +25,17 @@ FB.range(1, MAX_ITERATIONS).forEach(() => FB.usingLodash())
 console.timeEnd('usingLodash')
 
 console.time('parameterized')
-FB.range(1, MAX_ITERATIONS).forEach(() => FB.parameterized(NUMS, MUTATORS))
+FB.range(1, MAX_ITERATIONS).forEach(() => FB.parameterized(RANGE, MUTATORS))
 console.timeEnd('parameterized')
 
 console.time('functional')
-FB.range(1, MAX_ITERATIONS).forEach(() => FB.functional(NUMS, MUTATORS))
+FB.range(1, MAX_ITERATIONS).forEach(() => FB.functional(RANGE, MUTATORS))
 console.timeEnd('functional')
 
 console.time('events')
-FB.range(1, MAX_ITERATIONS).forEach(() => FB.events(NUMS, MUTATORS))
+FB.range(1, MAX_ITERATIONS).forEach(() => FB.events(RANGE, MUTATORS))
 console.timeEnd('events')
 
 console.time('promised')
-FB.range(1, MAX_ITERATIONS).forEach(() => FB.promised(NUMS, MUTATORS))
+FB.range(1, MAX_ITERATIONS).forEach(() => FB.promised(RANGE, MUTATORS))
 console.timeEnd('promised')
